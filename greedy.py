dc5f8fb22acc23e5a5b0a58bad61d9ffeefa034b
Kokoapo <viniviscdiniz@gmail.com>
2024-11-29 15:19:32 -0300

    Bug Fix

--- greedy.py
+++ greedy.py
@@ -1,5 +1,9 @@
-def greedy(values, weights, max_weight):                                            # 12n^2 + 15n + 4
-    size = len(values)
+def greedy(values, weights, max_weight):                                            # 19n^2 + 15n + 6
+    worth = []                                                                          # 7n + 2
+    for v, w in zip(values, weights):
+        worth.append(v/w)
+    
+    size = len(values)                                                                  # 2
     indexes = [False] * size                                                            # 3n + 2
     
     total_weight = 0                                                                    # 1
@@ -9,9 +13,9 @@ def greedy(values, weights, max_weight):                                        
 
         for i in range(size):                                                               # (2n + 2) + 10n = 12n + 2 
             if not indexes[i]:                                                                  # 2
-                if values[i] >= max_v and (weights[i] + total_weight) <= max_weight:            # 5
+                if worth[i] >= max_v and (weights[i] + total_weight) <= max_weight:            # 5
                     max_i = i                                                                   # 1
-                    max_v = values[i]                                                           # 2
+                    max_v = worth[i]                                                            # 2
         
         if max_i == -1:                                                                     # 1
             break                                                                           # 1
